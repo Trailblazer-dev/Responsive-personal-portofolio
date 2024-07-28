938cfbee4f4cd6fad539f50ea00cc28f2e8fb1e9
Trailblazer-dev <richvictor830@gmail.com>
2024-07-28 21:28:13 +0300

    building

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,25 @@ navLink.forEach(n => {
     });
     
 });
+
+// active
+
+const sections = document.querySelectorAll("section[id]");
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*="#' + sectionId + '"]')
+        .classList.add("active");
+    } else {
+      document
+        .querySelector('.nav__menu a[href*="#' + sectionId + '"]')
+        .classList.remove("active");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
